523543212f387986ebb9bfa3fb144c7b0681c74f
wupa9 <wupaz9@gmail.com>
2016-12-06 17:43:45 -0600

    Fix redux-form reducer name

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux'
-import { reducer as formReducer } from 'redux-form'
+import { reducer as form } from 'redux-form'
 import session from './session'
 
-const oaaChat = combineReducers({ session, formReducer })
+const oaaChat = combineReducers({ session, form })
 
 export default oaaChat
