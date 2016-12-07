c2a1bc8649b72a31b4907c2dd71d90a9fb64809e
wupa9 <wupaz9@gmail.com>
2016-12-07 15:11:56 -0600

    Setup material ui

--- app/components/Root.js
+++ app/components/Root.js
@@ -2,9 +2,16 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import App from './App'
 
+import injectTapEventPlugin from 'react-tap-event-plugin'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+
+injectTapEventPlugin()
+
 const Root = ({ store }) => (
 	<Provider store={store}>
-		<App />
+		<MuiThemeProvider>
+			<App />
+		</MuiThemeProvider>
 	</Provider>
 )
 
