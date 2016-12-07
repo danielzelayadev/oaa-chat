0bf372d7da96953f07f0bcd1db3ff86f7c68612d
wupa9 <wupaz9@gmail.com>
2016-12-07 15:11:30 -0600

    Refactor for redux-form data and better error checking

--- app/actions/session.js
+++ app/actions/session.js
@@ -5,12 +5,14 @@ export const LOGIN = 'LOGIN'
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
 export const LOGIN_FAILURE = 'LOGIN_FAILURE'
 
-export const login = () => async (dispatch, state) => {
+export const login = creds => async dispatch => {
 	dispatch({ type: LOGIN })
 	try {
-		const response = await axios.post(`http://localhost:8000/login`, state().form.login.values)
+		const response = await axios.post(`http://localhost:8000/login`, creds)
 		dispatch({ type: LOGIN_SUCCESS, token: response.data['token'] })
-	} catch ({ response }) {
-		dispatch({ type: LOGIN_FAILURE, error: response.data })
+	} catch (err) {
+		const response = err.response
+		const error = response ? response.data : err.message
+		dispatch({ type: LOGIN_FAILURE, error })
 	}
 }
