bd2f2b9b1dd05bb440b73d41e01bb1604c6a3571
wupa9 <wupaz9@gmail.com>
2016-12-08 02:19:55 -0600

    Add reducer for account activation

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux'
 import { reducer as form } from 'redux-form'
 import session from './session'
+import accountActivate from './accountActivate'
 
-const oaaChat = combineReducers({ session, form })
+const oaaChat = combineReducers({ 
+	session, accountActivate, form 
+})
 
 export default oaaChat
