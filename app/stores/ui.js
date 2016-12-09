64fc44626231411deb7b3a98ad17a60631a582a4
wupa9 <wupaz9@gmail.com>
2016-12-09 17:10:00 -0600

    Drawer management in ui store

--- app/stores/ui.js
+++ app/stores/ui.js
@@ -1,7 +1,24 @@
-import { observable } from 'mobx'
+import { observable, action, computed } from 'mobx'
 
 class UIStore {
+	@observable drawer
+	@observable drawerClosing = false
 
+	@action setDrawer (drawer) {
+		this.drawer = drawer
+	}
+
+	@action closeDrawer () {
+		this.drawerClosing = true
+		setTimeout(() => {
+			this.drawer = undefined
+			this.drawerClosing = false
+		}, 350)
+	}
+
+	@computed get drawerIsOpen () {
+		return drawer !== undefined
+	}
 }
 
-export default UIStore
+export default new UIStore
