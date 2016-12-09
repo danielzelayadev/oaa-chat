d023cc4d711cdb68b6bfd4b388d9b8465c736b31
wupa9 <wupaz9@gmail.com>
2016-12-09 00:51:42 -0600

    Regex constants

--- app/constants.js
+++ app/constants.js
@@ -2,3 +2,6 @@ import validatorjs from 'validatorjs'
 
 export const API = 'http://localhost:8000'
 export const plugins = { dvr: validatorjs }
+
+export const REGEX_EMAIL =  /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/igm
+export const REGEX_PASSWORD = /^(?=.*\d)(?=.*[A-Z]).+$/
