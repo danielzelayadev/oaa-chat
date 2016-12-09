030a90cd2aea32ed233dfcecca98b9ae57862a92
wupa9 <wupaz9@gmail.com>
2016-12-09 17:14:10 -0600

    Add and implement profile component

---
+++ app/components/Profile/Profile.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { DrawerLabel } from '..'
+import Avatar from 'material-ui/Avatar'
+import styles from './Profile.css'
+
+const avatarStyles = {
+	width: 200,
+	height: 200,
+	marginLeft: 88,
+    marginTop: 32,
+    marginRight: 88,
+    marginBottom: 6
+}
+
+class Profile extends Component {
+	render () {
+		const { user } = this.props.store
+		return (
+			<div class={styles.profileContent}>
+				<Avatar src={user.avatar} style={avatarStyles} />
+				<DrawerLabel name="Email" value={user.email} />
+				<DrawerLabel name="Username" value={user.username} />
+				<DrawerLabel name="Name" value={this.props.store.fullname} />
+				<DrawerLabel name="Age" value={user.age} />
+				<DrawerLabel name="Birthday" value={user.birthday} />
+			</div>
+		)
+	}
+}
+
+export default Profile
