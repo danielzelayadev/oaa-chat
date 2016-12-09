d55e96640a396830c77221ffe3efb7607ae5c2f7
wupa9 <wupaz9@gmail.com>
2016-12-09 02:52:17 -0600

    Change password regex to match the api requirements

--- app/constants.js
+++ app/constants.js
@@ -4,4 +4,4 @@ export const API = 'http://localhost:8000'
 export const plugins = { dvr: validatorjs }
 
 export const REGEX_EMAIL =  /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/igm
-export const REGEX_PASSWORD = /^(?=.*\d)(?=.*[A-Z]).+$/
+export const REGEX_PASSWORD = /^(?=.*[A-Z])(?=.*\d.*\d)[A-Za-z\d]{8,}$/
