f8e2d6e5b8d5d4dada25152ae3e70fd7adb030dd
wupa9 <wupaz9@gmail.com>
2016-12-10 07:13:48 -0600

    New utils for profile and drawer

--- app/utils.js
+++ app/utils.js
@@ -1,3 +1,16 @@
 export const trimTime = jsonDate => {
 	return jsonDate.slice(0, jsonDate.length - 14)
 }
+
+export const getUserProfileProps = user => ({
+	avatar: user.avatar,
+	details: [
+		{ name: "Email", value: user.email },
+		{ name: "Username", value: user.username },
+		{ name: "Name", value: `${user.firstname} ${user.lastname}` },
+		{ name: "Age", value: user.age },
+		{ name: "Birthday", value: user.birthday }
+	]
+})
+
+export const drawerIsOpen = (id, drawers) => id > 0 && id <= drawers
