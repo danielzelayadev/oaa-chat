dd030b23030cfa3a0c9a9687efb4688131070d4c
wupa9 <wupaz9@gmail.com>
2016-12-10 00:55:13 -0600

    Room name and avatar setup

--- app/components/NewRoom/NewRoom.js
+++ app/components/NewRoom/NewRoom.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 import { observer } from 'mobx-react'
+import { SessionStore, NewRoomStore } from '../../stores'
 import defaultAvatar from '../../res/default-group.png'
 import FloatingActionButton from 'material-ui/FloatingActionButton'
 import TextField from 'material-ui/TextField'
 import styles from './NewRoom.css'
+
+const avatarUploadId = "avatarUpload"
 
 const textFieldStyles = {
     width: '100%'
@@ -15,34 +18,42 @@ const submitStyles = {
     top: '75%'
 }
 
+const Test = observer(props => (<TextField {...props}/>))
+
 @observer class NewRoom extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			roomName: '',
-			showSubmit: false
+	componentWillUnmount() {
+		NewRoomStore.clear()
+	}
+
+	componentDidMount() {
+		const el = document.getElementById(avatarUploadId)
+		el.onchange = () => {
+			NewRoomStore.avatar = el.files[0]
+			NewRoomStore.readAvatarData()
 		}
 	}
 
-	updateRoomName (e) {
-		this.setState({ ...this.state, roomName: e.target.value,
-		                showSubmit: e.target.value.length > 0 })
+	uploadWindow (e) {
+		document.getElementById(avatarUploadId).click()
 	}
 
 	render () {
-		const { roomName, showSubmit } = this.state
+		const { avatarData, name } = NewRoomStore
+		const showSubmit = name.length > 0
 		return (
 			<div>
-				<div class={styles.avatar}>
-					<img src={defaultAvatar} class={styles.img} />
-					<div class={styles.avatarOverlay}>
+				<div class={styles.avatar} onClick={this.uploadWindow.bind(this)}>
+					<input id={avatarUploadId} type="file" style={{visibility: "hidden"}} />
+					<img src={ avatarData || defaultAvatar} class={styles.img} />
+					<div class={styles.avatarOverlay} 
+					      style={ avatarData ? { display: 'none' } : {} } >
 						<i class="material-icons">photo_camera</i>
 						<div class={styles.avatarOverlayText}>Add Room Avatar</div>
 					</div>
 				</div>
 				<div style={{ marginLeft: 32, marginTop: 26 }}>
-					<TextField floatingLabelText="Room Name" value={roomName} 
-							   onChange={this.updateRoomName.bind(this)} 
+					<TextField floatingLabelText="Room Name" value={name}
+							   onChange={e => NewRoomStore.name = e.target.value}
 					           style={textFieldStyles} />
 				</div>
 				<FloatingActionButton className={ showSubmit ? styles.showSubmit : styles.hideSubmit } 
