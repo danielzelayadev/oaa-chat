238492b6a5aba55ae7c55af1dd6d3291784e9796
wupa9 <wupaz9@gmail.com>
2016-12-17 02:12:09 -0600

    Remove default room

--- app/stores/rooms.js
+++ app/stores/rooms.js
@@ -16,8 +16,6 @@ class RoomsStore {
 			{ admin: 'kelvinosse', name: 'Mara Comida', members: [], messages: [], avatar: 'https://pbs.twimg.com/profile_images/378800000822867536/3f5a00acf72df93528b6bb7cd0a4fd0c.jpeg' },
 			{ admin: 'astrothing', name: '#ForgetRoberto', members: [], messages: [], avatar: 'https://cdn.thisiswhyimbroke.com/images/dog-mask-alt-640x533.jpg' },
 		]
-
-		this.openRoom = this.rooms[3]
 	}
 
 }
