477054b92026087e0b9835c4cc0bdeaa834f6ee3
wupa9 <wupaz9@gmail.com>
2016-12-16 21:06:13 -0600

    React emoji config

--- app/constants.js
+++ app/constants.js
@@ -5,3 +5,7 @@ export const plugins = { dvr: validatorjs }
 
 export const REGEX_EMAIL =  /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/igm
 export const REGEX_PASSWORD = /^(?=.*[A-Z])(?=.*\d.*\d)[A-Za-z\d]{8,}$/
+
+export const emojiConfig = {
+	emojiType: 'emojione'
+}
