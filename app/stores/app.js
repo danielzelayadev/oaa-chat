60448423ae56aa5f587ba393e8441d077576d06f
wupa9 <wupaz9@gmail.com>
2016-12-17 19:57:31 -0600

    App store for all app data

---
+++ app/stores/app.js
@@ -0,0 +1,19 @@
+import { observable, computed, action } from 'mobx'
+import axios from 'axios'
+import { SessionStore, UsersStore, RoomsStore } from '.'
+import { API } from '../constants'
+
+class AppStore {
+	@observable loading = false
+
+	@action async fetch() {
+		this.loading = true
+		await SessionStore.fetch()
+		await RoomsStore.fetch()
+		await UsersStore.fetch()
+		this.loading = false
+	}
+
+}
+
+export default new AppStore
