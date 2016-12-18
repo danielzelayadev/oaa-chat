9f576e5bc98f7bbbc2c4982effefb00c251b30c8
wupa9 <wupaz9@gmail.com>
2016-12-18 02:23:17 -0600

    Remove unnecessary caches

--- app/stores/rooms.js
+++ app/stores/rooms.js
@@ -1,18 +1,17 @@
 import { observable, computed, action } from 'mobx'
 import axios from 'axios'
 import { SessionStore } from '.'
-import { auth, get, cache } from '../utils'
+import { auth } from '../utils'
 import { API } from '../constants'
 
 class RoomsStore {
 	@observable openRoom = null
-	@observable rooms = get('rooms')
+	@observable rooms
 
 	@action async fetch () {
 		try {
 			const response = await axios.get(`${API}/rooms`, auth(SessionStore.token))
 			this.rooms = response.data
-			cache('rooms', this.rooms.slice())
 		} catch (e) {
 			console.error(e)
 		}
--- app/stores/users.js
+++ app/stores/users.js
@@ -1,17 +1,16 @@
 import { observable, computed, action } from 'mobx'
 import axios from 'axios'
 import { SessionStore } from '.'
-import { auth, get, cache } from '../utils'
+import { auth } from '../utils'
 import { API } from '../constants'
 
 class UsersStore {
-	@observable users = get('users')
+	@observable users
 
 	@action async fetch () {
 		try {
 			const response = await axios.get(`${API}/users`, auth(SessionStore.token))
 			this.users = response.data
-			cache('users', this.users.slice())
 		} catch (e) {
 			console.error(e)
 		}
